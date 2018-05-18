b4e4f9b5a79f08154daf227a096b2b536c23b62d
K.Kimura <dotnsf@gmail.com>
2018-05-18 10:40:13 +0900

    fix initial introduction with README.md

--- api/admintools/bn_setup.sh
+++ api/admintools/bn_setup.sh
@@ -2,6 +2,8 @@
 
 composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName r2b-network
 
-composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile ../r2b-network.bna --file ~/peerAdmin@hlfv1.card
+composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile ../r2b-network.bna --file ../cards/admin@r2b-network.card
+
+composer card import --file ../cards/admin@r2b-network.card
 
 composer network ping --card admin@r2b-network
